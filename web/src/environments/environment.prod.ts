ac5c2f9ade2d112012e7f9de43761158f0d7ba50
KevinMayfield <mayfield.g.kev@gmail.com>
2019-03-23 07:27:11 +0000

    Removed moment and vis libraries

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
     eprUrl : 'https://data.developer-test.nhs.uk/ccri-fhir/STU3',
     client_id : 'clinical-assurance-tool',
     client_secret : 'AM3ai-PGoZZRW-7osWbzvGlDBHjHq7M2aBlpNttreHeEyB5jequWy8fsHMVQP4JV0Kd0Fzrtu0iNEqGqguq69Qs',
-    cookie_domain : 'localhost',
+    cookie_domain : 'data.developer.nhs.uk',
     logonUrl: 'http://localhost:4200/ccri-logon'
   },
   keycloak: {
@@ -14,6 +14,6 @@ export const environment = {
     realm : 'ReferenceImplementations',
     client_secret : '709c79a1-7710-452f-859c-fb6edfb86027',
     client_id : 'ccri-cat',
-    cookie_domain : 'localhost'
+    cookie_domain : 'data.developer.nhs.uk'
   }
 };
